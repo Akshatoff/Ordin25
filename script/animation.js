77a7d188302df33bd9f6a25ae33c5995bf1e253b
Akshatoff <akshatascoder@gmail.com>
2025-02-05 15:15:55 +0530

    Card animation

---
+++ script/animation.js
@@ -0,0 +1,53 @@
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const third = document.getElementById("third");
+const fourth = document.getElementById("fourth");
+const fifth = document.getElementById("fifth");
+
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollTrigger)
+    // gsap code here!
+   });
+
+   gsap.fromTo(
+    ".card:not(:first-child)",
+    {
+        x: () => window.innerWidth / 2 + 500,
+        y: () => window.innerHeight / 2 + 100,
+        opacity: 0,
+
+
+    },
+    {
+        x: 0,
+        y: 0,
+        stagger: 0.5,
+        opacity: 1,
+        scrollTrigger: {
+            pin: ".container",
+            scrub: true,
+            start: "top top",
+            end: "+=5000",
+            invalidateOnRefresh: true
+
+        }
+    }
+   )
+
+
+// Initialize a new Lenis instance for smooth scrolling
+const lenis = new Lenis();
+
+// Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+lenis.on('scroll', ScrollTrigger.update);
+
+// Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+// This ensures Lenis's smooth scroll animation updates on each GSAP tick
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+});
+
+// Disable lag smoothing in GSAP to prevent any delay in scroll animations
+gsap.ticker.lagSmoothing(0);
